0be39d1577a4da2b5222907aa86079f5c6de2b39
r-sreesaran <r.sreesaran@gmail.com>
2018-05-06 04:25:15 +0530

    corrected code

--- client.py
+++ client.py
@@ -2,23 +2,29 @@ import urllib.request
 import json
 import ssl
 
-class client:
+class clientFactory:
 
     def __init__(self,userName,password):
         self.userName=userName
         self.password=password
-
+ 
+    def handlerIntializer(): 
 # importing a ssl certificate      
-      ctx = ssl.create_default_context()
-      ctx.check_hostname = False
-      ctx.verify_mode = ssl.CERT_NONE
+       ctx = ssl.create_default_context()
+       ctx.check_hostname = False
+       ctx.verify_mode = ssl.CERT_NONE
 
 # creating ssl handler
 
-      httpsHandler = urllib.request.HTTPSHandler(context=ctx)
+       httpsHandler = urllib.request.HTTPSHandler(context=ctx)
 
 # creating basicauth handler
 
-     password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
-     password_mgr.add_password(None,url,self.userName,self.password)
-     httpBasicAuthHandler = urllib.request.HTTPBasicAuthHandler(password_mgr)
+       password_mgr = urllib.request.HTTPPasswordMgr()
+       password_mgr.add_password(self.userName,self.password)
+       httpBasicAuthHandler = urllib.request.HTTPBasicAuthHandler(password_mgr)
+      
+       opener = urllib.request.build_opener(httpsHandler,httpBasicAuthHandler)
+       urllib.request.install_opener(opener)
+
+      
